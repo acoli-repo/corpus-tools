29ef22b14514c9562eec48c482a046424dbf5f94
maartenpt <maartenpt@gmail.com>
2016-05-04 09:00:54 +0200

    add CSV scripts

---
+++ common/Scripts/csv2tei.pl
@@ -0,0 +1,193 @@
+use XML::LibXML;
+use Getopt::Long;
+use XML::XPath;
+use XML::XPath::XMLParser;
+$\ = "\n"; $, = "\t";
+
+# Tab separated text to TEI
+# A script to load a CSV table in UTF8
+# And fills the information in each row into a TEI file
+
+$parser = XML::LibXML->new(); 
+
+GetOptions (
+            'csvfile=s' => \$csvfile,
+            'teiheader=s' => \$teiheader,
+            'xmlfolder=s' => \$xmlfolder,
+            'debug' => \$debug,
+            'notext' => \$notext,
+            'template=s' => \$template,
+            'queries=s' => \$xpathqueries,
+            'header' => \$header, # Whether the CSV file has a (non XPath) header row
+            );
+
+# Check whether the conditions are met
+if ( !$xmlfolder ) { $xmlfolder = "xmlfiles"; };
+if ( !$csvfile ) { $csvfile = shift; };
+if ( !$csvfile ) { print "Usage: csv2tei [options] csvfile"; exit; };
+if ( !-e $csvfile ) { print "No such file: $csvfile"; exit; }; 
+
+# Open the CSV file
+open FILE, $csvfile;
+binmode ( FILE, ":utf8" );
+
+# read the header line(s)
+if ( $header ) { 
+	$headerrow = <FILE>; chomp($headerrow); 
+	@namefields = split ( "\t", $headerrow );
+	if ( $debug ) { print "Field names:\n".join ( "\n", @namefields); };
+};
+
+if ( $xpathqueries ) {
+	open XFILE, $xpathqueries; 
+	binmode ( XFILE, ":utf8" );
+	$xpathqueries = <XFILE>;
+	chop ( $xpathqueries );
+	close XFILE;
+	@xpfields = split ( ",", $xpathqueries ); 
+	unshift(@xpfields, "[fn]");
+	if ( $debug ) { print "Xpath definitions:\n".join ( "\n", @xpfields); };
+} elsif ( $teiheader ) {
+} else {
+	$xprow = <FILE>; chomp($xprow);
+	@xpfields = split ( "\t", $xprow );
+	if ( $debug ) { print "Xpath definitions:\n".join ( "\n", @xpfields); };
+};
+
+$filecnt = 0;
+
+# Read the actual lines
+while ( <FILE> ) {
+	chomp; $line++; $fn = ""; undef(%toset);
+	$filecnt++;
+	@row = split ( "\t" );
+	if ( $debug ) { print "Line $line"; };
+	
+	# See what we need to do with this row
+	for ( $i=0; $i<scalar @row; $i++ ) {
+		$xpath = $xpfields[$i];
+		if ( $xpath eq "[fn]" ) { 
+			$fn = $row[$i];
+		} elsif ( $xpath =~ /^\// ) {
+			if ( $row[$i] ne ''  ) {
+				$toset{$xpath} = $row[$i];
+				if ( $debug ) { print $row[$i], $xpath; };
+			};
+		} else {
+			print " -- Unparsable definition: $xpath"; exit;
+		};
+	};
+	
+	# Check the filename or use the sequential number
+	if ( $fn eq '' ) { 
+		$fn = $filecnt;
+	};
+	if ( $fn !~ /\.xml$/ ) { $fn .= ".xml"; };
+	if ( $fn !~ /\// ) {
+		$fn = "$xmlfolder/$fn";
+	};
+	
+	print "Treating: $fn";
+	# See if the file exists or create a new TEI XML
+	if ( -e $fn) {
+		eval {
+			$xml = $parser->load_xml(location => $fn);
+		};
+		if ( !$xml ) { print "Unable to parse: $fn"; exit; };
+	} else {
+		if ( $template ) {
+			$tei = $template;
+		} elsif ( $notext ) {
+			$tei = "<TEI/>";
+		} else { 
+		$tei = "<TEI>
+<teiHeader>
+</teiHeader>
+<text>
+</text>
+</TEI>";
+		};
+		$xml = $parser->load_xml(string => $tei);
+	};	
+
+	# Now load the actual values into the XML
+	while ( ( $xpath, $xval ) = each ( %toset ) ) {
+		xpathset ( $xpath, $xml, $xval );
+	};
+	
+	open OUTFILE, ">$fn";
+	print OUTFILE $xml->toString;
+	close OUTFILE;
+	
+};
+
+sub xpathset ( $xquery, $xml, $val ) {
+	( $xquery, $xml, $val ) = @_;
+	
+	@tmp = $xml->findnodes($xquery); 
+	if ( @tmp ) { 
+		# Node exists
+		$thisnode = shift(@tmp);
+			if ( $thisnode->nodeType == 2 ) {
+				# Attribute node
+				$thisnode->setValue($val);
+				if ( $debug ) { print "Setting attribute value: $xquery = $val"; };
+			} else {
+				$newnode = $xml->createTextNode( $val );
+				$thisnode->addChild($newnode);
+				if ( $debug ) { print "Adding text content: $xquery = $val"; };
+			};
+	} else {
+		# Node not found - create it
+		if ( $xquery =~ /^(.*)\/(.*?)$/ ) {
+			$parxp = $1; $thisname = $2;
+			if ( $thisname =~ /^@(.*)/ ) {
+				# Attribute node
+				$thisname = $1; 
+				$parnode = makenode($xml, $parxp);
+				$parnode->setAttribute($thisname, $val);
+			} else {
+				$thisnode = makenode($xml, $xquery);
+				$newnode = $xml->createTextNode( $val );
+				$thisnode->addChild($newnode);
+			};
+		};
+	};
+};
+
+sub makenode ( $xml, $xquery ) {
+	my ( $xml, $xquery ) = @_;
+	@tmp = $xml->findnodes($xquery); 
+	if ( scalar @tmp ) { 
+		$node = shift(@tmp);
+		if ( $debug ) { print "Node exists: $xquery"; };
+		return $node;
+	} else {
+		if ( $xquery =~ /^(.*)\/(.*?)$/ ) {
+			my $parxp = $1; my $thisname = $2;
+			my $parnode = makenode($xml, $parxp);
+			$thisatts = "";
+			if ( $thisname =~ /^(.*)\[(.*?)\]$/ ) {
+				$thisname = $1; $thisatts = $2;
+			};
+			$newchild = XML::LibXML::Element->new( $thisname );
+			
+			# Set any attributes defined for this node
+			if ( $thisatts ne '' ) {
+				if ( $debug ) { print "setting attributes $thisatts"; };
+				foreach $ap ( split ( " and ", $thisatts ) ) {
+					if ( $ap =~ /\@([^ ]+) *= *"(.*?)"/ ) {
+						$an = $1; $av = $2; 
+						$newchild->setAttribute($an, $av);
+					};
+				};
+			};
+
+			if ( $debug ) { print "Creating node: $xquery ($thisname)"; };
+			$parnode->addChild($newchild);
+			
+		} else {
+			print "Failed to find or create node: $xquery";
+		};
+	};
+};
---
+++ common/Scripts/xml2csv.pl
@@ -0,0 +1,84 @@
+use XML::LibXML;
+use Getopt::Long;
+use XML::XPath;
+use XML::XPath::XMLParser;
+
+# TEI to tab separated text 
+# A script to create a CSV table in UTF8
+# And read the information in each row from a TEI file
+
+$parser = XML::LibXML->new(); 
+
+GetOptions (
+            'csvfile=s' => \$csvfile,
+            'xmlfolder=s' => \$xmlfolder,
+            'queries=s' => \$xpathqueries,
+            'extention=s' => \$ext,
+            'debug' => \$debug,
+            );
+
+if ( $xmlfolder eq '' ) { $xmlfolder = "xmlfiles"; };
+
+if ( $xpathqueries eq '' ) { 
+	$xpathqueries = "//title"; 
+} elsif ( -e $xpathqueries ) {
+	open FILE, $xpathqueries; 
+	binmode ( FILE, ":utf8" );
+	$xpathqueries = <FILE>;
+	chop ( $xpathqueries );
+	close FILE;
+};
+if ( $ext eq '' ) { $ext = "xml"; };
+
+if ( $csvfile eq '' ) { 
+	*OUTPUT = *STDOUT;
+} else {
+	open OUTPUT, ">$csvfile" or die("Unable to open $csvfile\n");
+	print "Saving to $csvfile\n";
+};
+binmode ( OUTPUT, ":utf8" );
+
+@xpath = split ( ",", $xpathqueries);
+readfolder($xmlfolder);
+
+close OUTPUT;
+
+# Go recursively through a folder
+sub readfolder ( $folder ) {
+	my $folder = @_[0]; 
+	
+	opendir(my $dh, $folder) || die ("Cannot read: $folder");
+	while(readdir $dh) {
+		$file = $_;
+		if ( $file =~ /^\./ ) {	
+			next;
+		} elsif ( -d $folder.'/'.$file ) { 
+			readfolder ($folder.'/'.$file);
+		} elsif ( $file =~ /\.$ext/ )  {
+			treatfile ( $folder.'/'.$file );
+		};
+	}
+	closedir $dh;
+}
+
+sub treatfile ( $file ) {
+	my $file = @_[0]; 
+	
+	$xml = $parser->load_xml(location => $file);
+	
+	print OUTPUT $file;
+	foreach $xpath ( @xpath ) {	
+		@tmp = $xml->findnodes($xpath."");
+		print OUTPUT "\t";
+		if ( @tmp ) {
+			print OUTPUT $tmp[0]->textContent;
+		} else {
+			if ( $debug ) {
+				print OUTPUT "*".$xpath;
+			} else {
+				print OUTPUT "";
+			};
+		};
+	};	
+	print OUTPUT "\n";
+}
