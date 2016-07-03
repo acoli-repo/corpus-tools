5900b6e79549da08df374181d375e5d775404576
maartenpt <maartenpt@gmail.com>
2016-07-03 21:53:36 +0200

    minor changes

--- common/Sources/deptree.php
+++ common/Sources/deptree.php
@@ -16,18 +16,42 @@
 
 	if ( $sid ) {
 
-
 		$sent =	current($ttxml->xml->xpath("//s[@id='$sid']"));
+		$fileid = $ttxml->fileid;
 
-		$maintext .= "<div id=mtxt>".$sent->asXML()."</div><hr>";
+		# Find the next/previous sentence (for navigation)
+		$nextsent = current($sent->xpath("following::s")); 
+		if ( $nextsent ) {
+			$nid = $nextsent['id'];
+			$nexts = $nextsent['n'] or $nexts = $nid;
+			$nnav = "<a href='index.php?action=$action&cid=$fileid&sid=$nid'>> $nexts</a>";
+		};
+		$prevsent = current($sent->xpath("preceding::s[1]")); 
+		if ( $prevsent ) {
+			$pid = $prevsent['id'];
+			$prevs = $prevsent['n'] or $prevs = $pid;
+			$bnav = "<a href='index.php?action=$action&cid=$fileid&sid=$pid'>$prevs <</a>";
+		};
+		
+		$pagenav = "<table style='width: 100%'><tr> <!-- /<$pbelm [^>]*id=\"{$_GET['pageid']}\"[^>]*n=\"(.*?)\"/ -->
+						<td style='width: 33%' align=left>$bnav
+						<td style='width: 33%' align=center>{%sentence} $sid
+						<td style='width: 33%' align=right>$nnav
+						</table>
+						<hr>
+						";
+
+		$maintext .= "		$pagenav
+			<div id=mtxt>".$sent->asXML()."</div><hr>";
 
 		$graph = drawgraph($sent);
 		$width = $xpos + 50;
+		$height = $width/3;
 	
 		$maintext .= "\n
 <script language=\"Javascript\" src=\"$jsurl/tokview.js\"></script>
 <script language=\"Javascript\" src=\"$jsurl/tokedit.js\"></script>
-<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"$width\" height=\"300\">
+<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"$width\" height=\"$height\">
 	<defs>
 		<marker id=\"arrow\" markerWidth=\"10\" markerHeight=\"10\" refx=\"0\" refy=\"3\" orient=\"auto\" markerUnits=\"strokeWidth\">
 			<path d=\"M0,0 L0,6 L9,3 z\" fill=\"#f00\" />
@@ -87,8 +111,13 @@
 				$h = floor(abs($w/2.5));  $os = floor($w/8);
 				$y2 = $y1 + $h;
 				$r1 = $x1+$os; $r2 = $x2-$os;
-				$treetxt .= "\n\t<path d=\"M$x1 $y1 C $r1 $y2, $r2 $y2, $x2 $y1\" stroke=\"black\" fill=\"transparent\" marker-end=\"url(#arrow)\"/>";
+				$treetxt .= "\n\t<path title=\"{$tok['deps']}\" d=\"M$x1 $y1 C $r1 $y2, $r2 $y2, $x2 $y1\" stroke=\"black\" fill=\"transparent\" marker-end=\"url(#arrow)\"/>";
 				$treetxt .= "<circle cx=\"$x1\" cy=\"$y1\" r=\"2\" stroke=\"black\" stroke-width=\"1\" fill=\"black\" />";
+// 				if ( $tok['deps'] ) {
+// 					$lx = $x1 + $w/2;
+// 					$ly = $y2;
+// 					$treetxt .= "\n\t<text x=\"$lx\" y=\"$ly\" font-family=\"Courier\" font-size=\"12\">{$tok['deps']}</text> ";
+// 				};
 			};
 		};
 				
