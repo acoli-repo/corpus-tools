76e34ae78e9a49fa3de058996cc7a26eb067e164
maartenpt <maartenpt@gmail.com>
2016-11-06 12:50:48 +0100

    minor changes

---
+++ common/Sources/igt.php
@@ -0,0 +1,48 @@
+<?php
+	
+	# Basic support for IGT format, where morphemic data are stored in //tok/morph
+	# (c) Maarten Janssen, 2016
+
+	require ("../common/Sources/ttxml.php");
+	
+	$ttxml = new TTXML();
+	$cid = $ttxml->fileid;
+	
+	$maintext .= "<h1>".$ttxml->title()."</h1>"; 
+
+	# Display the teiHeader data as a table
+	$maintext .= $ttxml->tableheader(); 
+
+	$maintext .= "<div id=mtxt>";
+	$maintext .= "<style>.floatbox { float: left; margin-right: 10px; }</style>";
+	
+	foreach ( $ttxml->xml->xpath("//s") as $sent ) {
+		$maintext .= "<table id=$sid><tr><td style='border-right: 1px solid #bbaabb;'>";
+		$maintext .= "<div class='floatbox' id='$sid' style='font-weight: bold; padding-right: 5px;'>Word<table>";
+		foreach ( $settings['annotations']['morph'] as $item ) {
+			if (is_array($item)) $maintext .= "<tr><td style='color:{$item['color']}; font-weight: bold;'>".$item['display'];
+		};
+		$maintext .= "</table></div><td style='padding-left: 5px;'>";		
+		
+		foreach ( $sent->xpath(".//tok") as $tok ) {
+			$maintext .= "<div class=floatbox id=$sid>".$tok->asXML()."<table>";
+			foreach ( $settings['annotations']['morph'] as $item ) {
+				if ( !is_array($item) ) continue;
+				$maintext .= "<tr>";
+				foreach ( $tok->xpath(".//morph") as $morph ) {
+					$maintext .= "<td align=center title='{$item['display']}'  style='color: {$item['color']};'>".$morph[$item['key']];
+				};
+			};
+			$maintext .= "</table></div>";		
+		};
+		foreach ( $settings['xmlfile']['sattributes']['s'] as $item ) {
+			if ( !is_array($item) ) continue;
+	 		$maintext .= "<tr><td style='border-right: 1px solid #bbaabb; color: {$item['color']}'>{$item['short']}</td><td style='padding-left: 5px; color: {$item['color']}'> ".$sent[$item['key']]."</td>";
+		};
+		$maintext .= "</div>";
+	};
+	$maintext .= "</table></div><hr>";
+			$maintext .= "<a href='index.php?action=edit&cid=$cid&jmp=$sentid'>{%to text mode}</a> $options</p><br>";
+
+
+?>
