a251ae825f234d7ff5078ea663330d51c3ad76b2
maartenpt <maartenpt@gmail.com>
2016-11-07 14:52:21 +0100

    minor changes

--- common/Sources/igt.php
+++ common/Sources/igt.php
@@ -17,25 +17,33 @@
 	$maintext .= "<style>.floatbox { float: left; margin-right: 10px; }</style>";
 	
 	foreach ( $ttxml->xml->xpath("//s") as $sent ) {
+		$morphed = 0; if ( $sent->xpath(".//morph") ) { $morphed = 1; };
 		$maintext .= "<table id=$sid><tr><td style='border-right: 1px solid #bbaabb;'>";
 		$maintext .= "<div class='floatbox' id='$sid' style='padding-right: 5px;'>Word";
-		$maintext .= "<table style='margin: 0;'>";
-		foreach ( $settings['annotations']['morph'] as $item ) {
-			if (is_array($item)) $maintext .= "<tr><td style='color:{$item['color']};'>".$item['display'];
+		if ( $morphed ) {
+			$maintext .= "<table style='margin: 0;'>";
+			foreach ( $settings['annotations']['morph'] as $item ) {
+				if (is_array($item)) $maintext .= "<tr><td style='color:{$item['color']};'>".$item['display'];
+			};
+			$maintext .= "</table>";
 		};
-		$maintext .= "</table></div><td style='padding-left: 5px;'>";		
+		$maintext .= "</div><td style='padding-left: 5px;'>";		
 		
 		foreach ( $sent->xpath(".//tok") as $tok ) {
-			$maintext .= "<div class=floatbox id=$sid>".$tok->asXML()."<table style='margin: 0;'>";
-			foreach ( $settings['annotations']['morph'] as $item ) {
-				if ( !is_array($item) ) continue;
-				$maintext .= "<tr>";
-				foreach ( $tok->xpath(".//morph") as $morph ) {
-					$txt = $morph[$item['key']]; if ( $txt == '' ) { $txt = "&nbsp;"; };
-					$maintext .= "<td align=center title='{$item['display']}'  style='color: {$item['color']};'>$txt</td>";
+			$maintext .= "<div class=floatbox id=$sid>".$tok->asXML();
+			if ( $morphed ) {
+				$maintext .= "<table style='margin: 0;'>";
+				foreach ( $settings['annotations']['morph'] as $item ) {
+					if ( !is_array($item) ) continue;
+					$maintext .= "<tr>";
+					foreach ( $tok->xpath(".//morph") as $morph ) {
+						$txt = $morph[$item['key']]; if ( $txt == '' ) { $txt = "&nbsp;"; };
+						$maintext .= "<td align=center title='{$item['display']}'  style='color: {$item['color']};'>$txt</td>";
+					};
 				};
+				$maintext .= "</table>";
 			};
-			$maintext .= "</table></div>";		
+			$maintext .= "</div>";		
 		};
 		foreach ( $settings['xmlfile']['sattributes']['s'] as $item ) {
 			if ( !is_array($item) ) continue;
