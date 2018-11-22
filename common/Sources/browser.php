1b0ed44fe6035b37c2600a734a4f736f1efcb76a
maartenpt <maartenpt@gmail.com>
2018-11-22 11:56:22 +0100

    minor changes

--- common/Sources/browser.php
+++ common/Sources/browser.php
@@ -23,16 +23,26 @@
 		$cqpquery = "Matches = <text> [] :: match.text_$class = '$val'";
 		$cqp->exec($cqpquery);
 
+		$oval = $val;
+		if ( $val == "" || $val == "_" ) $val = "({%none})";
+		else if ( $item['type'] == "kselect" || $item['translate'] ) $val = "{%$class-$val}";
+
 		$size = $cqp->exec("size Matches");
-		$maintext .= "<p><a href='index.php?action=$action'>{%Documents}</a> > <a href='index.php?action=$action&class=$class'>{%$cat}</a> > $val
-			<p>$size {%documents} $nav
-			<hr>";
 
 		$max = $_GET['max'] or $max = 100;
 		$start = $_GET['start'] or $start = 0;
 		$stop = $start + $max;
-		if ( $size > $max ) $nav = " - {%showing} $start - $stop";
-		
+		if ( $size > $max || $start > 0 ) {
+			$next = $stop; $beg = $start + 1; $prev = max(0, $start - $max);
+			if ( $start > 0 ) $bnav .= " <a href='index.php?action=$action&class=$class&val=$oval&start=$prev'>{%previous}</a> ";
+			if ( $size > $max ) $bnav .= " <a href='index.php?action=$action&class=$class&val=$oval&start=$next'>{%next}</a> ";
+			$nav = " - {%showing} $beg - $stop - $bnav";
+		};
+
+		$maintext .= "<p><a href='index.php?action=$action'>{%Documents}</a> > <a href='index.php?action=$action&class=$class'>{%$cat}</a> > $val
+			<p>$size {%documents} $nav
+			<hr>";
+	
 		if ( $size > 0 ) {
 			$catq = "tabulate Matches $start $stop match text_id, match text_title";
 			// $maintext .= "<p>$cqpquery; $catq;";
