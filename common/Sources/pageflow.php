42fde4cf6d0413135b3019d297e635f6ebc190e8
Maarten Janssen <maartenpt@gmail.com>
2019-09-27 14:23:03 +0200

    minor changes

--- common/Sources/pageflow.php
+++ common/Sources/pageflow.php
@@ -77,7 +77,7 @@
 			$header
 			</div>
 		</div>
-		<div id='viewport' style='position: fixed; top: 0; width: 100%;'>
+		<div id='viewport' style='z-index: 160; position: fixed; top: 0; width: 100%;'>
 		<table width='100%' height='500px' style='table-layout: fixed;' id=viewtable>
 		   <colgroup>
 			<col id='col1' style='width: 50%'>
@@ -102,7 +102,7 @@
 			</tr>
 			<tr style='height: 30px; overflow: hidden;'>
 			<td colspan=3>
-				<div id='options' style='overflow: scroll;'>
+				<div id='options' style='z-index: 200; overflow: scroll;'>
 					$viewoptions
 					$showoptions
 					- {%Switch to view}: <select onChange=\"window.open(this.value, '_self');\">$viewsels</select>
