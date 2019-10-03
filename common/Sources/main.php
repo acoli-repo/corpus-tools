b74d672905899025903c975937ef06494abe0080
Maarten Janssen <maartenpt@gmail.com>
2019-10-03 12:08:29 +0200

    minor changes

--- common/Sources/main.php
+++ common/Sources/main.php
@@ -146,6 +146,11 @@
 	
 	$sharedfolder = $settings['defaults']['shared']['folder'];
 	
+	# Use the shared template if no local one exists
+	if (  !file_exists("templates/main.tpl") && file_exists ("$sharedfolder/templates/main.tpl") ) {
+		$smarty->setTemplateDir('$sharedfolder/templates');
+	};
+	
 	## Determine which action to perform
 	if ( file_exists( "Pages/$action-$lang.html" ) ) {
 		# Local page - language depedent
