8e4bf2ac0de4c25230d40e8f4ee3107a2e68137d
Maarten Janssen <maartenpt@gmail.com>
2019-11-26 10:53:55 +0100

    minor changes

--- common/Sources/context.php
+++ common/Sources/context.php
@@ -48,7 +48,7 @@
 
 	$headtext = $settings['context']['link'] or $headtext = "View TEITOK document";
 	if ( $withheader ) {
-		if ( $withlang ) $headtext = lgMsg("{%headtext}"; ); 
+		if ( $withlang ) $headtext = lgMsg("{%headtext}"); 
 		$header = "<p style='margin-top: 30px;'><a href='{$baseurl}index.php?action=file&cid=$cid&tid=$tid'>$headtext</a></p>";
 	};
 
