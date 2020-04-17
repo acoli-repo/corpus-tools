e6c5a16c8155bbd655503ddf896e847cd2cb73f5
Maarten Janssen <maartenpt@gmail.com>
2020-04-17 16:40:07 +0200

    Built a check that context.php expands to existing region

--- common/Sources/context.php
+++ common/Sources/context.php
@@ -11,7 +11,7 @@
 	if ( isset($_GET['header']) ) $withheader = $_GET['header']; else if ( isset($settings['context']['header']) ) $withheader = $settings['context']['header']; else $withheader = 1;
 	if ( isset($_GET['wordh']) ) $withword = $_GET['wordh']; else if ( isset($settings['context']['wordheader']) ) $withword = $settings['context']['wordheader']; else $withword = 0; # Whether to display the word itself
 	$context = $_GET['context'] or $context = $settings['context']['context'] or $context = "s";
-	if ( $context + 0 == 0 ) {
+	if ( $context + 0 == 0 && !$settings['cqp']['sattributes'][$context] ) {
 		if ( $username ) fatal("Context set to $context, which is not a CQP level in this corpus. Please correct in settings.xml//context");
 		$context = 5;
 	};      
