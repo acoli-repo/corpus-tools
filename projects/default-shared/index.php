0010b265d0ce3bde920dbe948bb37511dd5e58dd
Maarten Janssen <maartenpt@gmail.com>
2020-04-18 10:44:29 +0200

    Added a default shared project

---
+++ projects/default-shared/index.php
@@ -0,0 +1,16 @@
+<?php
+
+	// define where the TEITOK common files can be found
+	$ttroot = getenv('TT_ROOT') or "..";
+
+	// define which time-zone to use (obligatory for date in PHP)
+	date_default_timezone_set('UTC');
+	
+	// define which errors to report
+	ini_set('display_errors', '0');
+	error_reporting(E_ERROR|E_WARNING);
+
+	// call the main php script and only use resources from here
+	include ( "$ttroot/common/Sources/main.php" );
+
+?>
