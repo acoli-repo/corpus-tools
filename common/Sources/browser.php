80deb184d5f1c288094d44e56d847f31049337d8
Maarten Janssen <maartenpt@gmail.com>
2020-08-31 23:09:21 +0200

    corrected link in browser

--- common/Sources/browser.php
+++ common/Sources/browser.php
@@ -138,7 +138,7 @@
 							} else $fatts[$key] = "{%$attit-$fatt}";
 						};
 					};
-					$maintext .= "<tr><td><a href='index.php?action=$fileview&cid={$fid}'>{$fidtxt}</a><td style='padding-left: 6px; padding-right: 6px; border-left: 1px solid #dddddd;'>".join ( "<td style='padding-left: 6px; padding-right: 6px; border-left: 1px solid #dddddd;'>", $fatts );
+					$maintext .= "<tr><td><a href='index.php?action=file&cid={$fid}'>{$fidtxt}</a><td style='padding-left: 6px; padding-right: 6px; border-left: 1px solid #dddddd;'>".join ( "<td style='padding-left: 6px; padding-right: 6px; border-left: 1px solid #dddddd;'>", $fatts );
 				};
 				$maintext .= "</table>";
 
