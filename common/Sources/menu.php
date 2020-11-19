a60f5497707d7b12b824ff0e1b775cfca45d8207
Maarten Janssen <maartenpt@gmail.com>
2020-11-19 12:05:15 +0100

    added title for menu items

--- common/Sources/menu.php
+++ common/Sources/menu.php
@@ -36,7 +36,8 @@
     $menu .= "<ul style='text-align: left'>";
         	
     foreach ( $settings['menu']['itemlist'] as $key => $item ) { 
-    	if ( substr($key,0,4) == "http" ) $link = $key;
+    	if ( $item['link'] ) $link = $item['link'];
+    	else if ( substr($key,0,4) == "http" ) $link = $key;
     	else $link = "{$tlpr}index.php?action=$key";
     	if ( preg_replace("/&.*/", "", $key) == $action ) {
     		$scl = " class='selected'"; 
@@ -46,6 +47,7 @@
     		$scli = "";
     	};
 		$itemtxt = $item['display'] or $itemtxt = $key;
+		if ( $item['title'] ) $scl .= " title=\"{$item['title']}\"";
     	if ( $item['type'] == "separator" ) {
     		$menu .= "</ul><hr><ul style='text-align: left'>";
     	} else if ( $item['type'] == "header" ) {
