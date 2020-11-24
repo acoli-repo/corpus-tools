2976913df38bb1fa8fcbfa8157e28d3034f088c4
Maarten Janssen <maartenpt@gmail.com>
2020-11-24 10:44:15 +0100

    added logout to extuser

--- common/Sources/extuser.php
+++ common/Sources/extuser.php
@@ -4,10 +4,16 @@
 
 	if ( $user['email']) {
 		$maintext .= "<p>You are currently logged in as corpus administrator {$user['realname']}";
+		$logintype = "admin";
 	} else if ( $_SESSION['extid']['realname'] ) {
 		$maintext .= "<p>You are currently logged in as visitor {$_SESSION['extid']['realname']}";
+		$logintype = "guest";
 	} else {
 		$maintext .= "<p>You are not currently logged in";
+	};
+	
+	if ( $logintype ) {
+		$maintext .= "<p><a href='index.php?action=login&act=exit'>logout</a>";
 	};
 
 ?>
