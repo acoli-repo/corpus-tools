bf6b705bfbbdadc28bb5de577558ec8db2adb4e2
Maarten Janssen <maartenpt@gmail.com>
2021-01-12 18:12:19 +0100

    added rsearch and rglob

--- common/Sources/functions.php
+++ common/Sources/functions.php
@@ -63,6 +63,23 @@
 
 		return $filepath;
 	};
+	function rsearch($folder, $pattern) {
+		$dir = new RecursiveDirectoryIterator($folder);
+		$ite = new RecursiveIteratorIterator($dir);
+		$files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
+		$fileList = array();
+		foreach($files as $file) {
+			$fileList = array_merge($fileList, $file);
+		}
+		return $fileList;
+	};
+	function rglob($pattern, $flags = 0) {
+		$files = glob($pattern, $flags); 
+		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+			$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+		}
+		return $files;
+	};
 
 	function getxmlrec($fn, $id, $tag, $opt="") {
 		# Get a record from an XML file by ID on a given tag
