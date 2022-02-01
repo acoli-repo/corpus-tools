7fa3d0274a9dfa797fab67bdffc54faac3544fda
Maarten Janssen <maartenpt@gmail.com>
2022-02-01 18:28:15 +0100

    attempt to improve sessionrenew

--- Scripts/sessionrenew.js
+++ Scripts/sessionrenew.js
@@ -1,22 +1,28 @@
 var sessiontest = true;
 var testloop = window.setInterval(function() {
-  var el=document.createElement('img');
-  el.src='index.php?action=sessionrenew&rand='+Math.random();
-  el.style.opacity=.01;
-  el.style.width=1;
-  el.style.height=1;
-  el.onload=function() {	
-    document.body.removeChild(el);
-  }
-  el.onerror=function(e) {	
-  	console.log('session check failed');
-  	console.log(e);
-  	if ( sessiontest ) {
-  		alert('You seem to have been logged out from TEITOK - copy any relevant material you might be loosing, and renew your session in another window');
-    	clearInterval(testloop);
-    };
-    sessiontest = false;
-    document.body.removeChild(el);
-  }
-  document.body.appendChild(el);
+	var xhr = new XMLHttpRequest();
+  	var url = 'index.php?action=sessionrenew&type=text&rand='+Math.random();
+
+	xhr.open("GET", url);
+
+	// xhr.setRequestHeader("Accept", "application/json");
+
+	xhr.onreadystatechange = function () {
+	   if (xhr.readyState === 4) {
+	      var stat = xhr.responseText.trim();
+	      if ( stat == "logged out" ) {
+	      	console.log('logged out');
+			if ( sessiontest ) {
+				alert('You seem to have been logged out from TEITOK - copy any relevant material you might be loosing, and renew your session in another window');
+				clearInterval(testloop);
+			};
+			sessiontest = false;
+	      } else if ( stat == "logged in" ) {
+			// console.log('ok');
+	      } else {
+			console.log('?? ' + stat);
+	      };
+	   }};
+
+	xhr.send();
 }, 30000);
--- common/Sources/sessionrenew.php
+++ common/Sources/sessionrenew.php
@@ -1,13 +1,29 @@
 <?php
 
 	// PHP script to keep the session alive when doing potentially long edits
-	if ( $username != "" ) {
-		// logged in - provide an image
-		header('Content-Type: image/png');
-		echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=');
+	if ( $_GET['type'] == "text" ) {
+		header('Content-Type: text/plain');
+		if ( $username != "" ) {
+			print "logged in";
+		} else {
+			print "logged out";
+		};
+	} else if ( $_GET['type'] == "json" ) {
+		header('Content-Type: text/json');
+		if ( $username != "" ) {
+			print "{'message': 'logged in'}";
+		} else {
+			print "{'error': 'logged out'}";
+		};
 	} else {
-		// logged out - throw an error
-		header("HTTP/1.0 403 Forbidden");
+		if ( $username != "" ) {
+			// logged in - provide an image
+			header('Content-Type: image/png');
+			echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=');
+		} else {
+			// logged out - throw an error
+			header("HTTP/1.0 403 Forbidden");
+		};
 	};
 	exit;
 
