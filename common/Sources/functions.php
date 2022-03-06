12d4a53b27d04f99cba5c40452eb3ef751dbb5f2
Maarten Janssen <maartenpt@gmail.com>
2022-03-06 23:13:29 +0100

    added base to tmp/settings

--- common/Sources/functions.php
+++ common/Sources/functions.php
@@ -1071,8 +1071,8 @@
 		# And we need to copy the base for the URL
 		if ( $settings['defaults']['base']['url'] ) {
 			$deff = $merged->addChild("default");
-			$basef = $deff->addChild("base");
-			$basef->setAttribute("url", $settings['defaults']['base']['url']);
+			$basef[ = $deff->addChild(]"base");
+			$basef["url"] = $settings['defaults']['base']['url'];
 		};
 		
 		return $merged;
