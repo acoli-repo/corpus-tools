68b79fdd8e9372610b7c3511aeb60be26aba51b3
Maarten Janssen <maartenpt@gmail.com>
2022-09-16 09:55:31 +0200

    improved renumber.pl

--- common/Scripts/xmlrenumber.pl
+++ common/Scripts/xmlrenumber.pl
@@ -38,6 +38,7 @@ use XML::LibXML;
 	if ( $mtxtelem eq '' ) { $mtxtelem = "//text"; }; # Do we want this?
 	if ( $mtxtelem ne '' && $mtxtelem !~ /\// ) { $mtxtelem = "//$mtxtelem"; };
 
+
 	# Find the last token number
 	$max = 0; 
 	foreach $ttnode ($tmpdoc->findnodes("//tok")) {
@@ -58,6 +59,7 @@ use XML::LibXML;
 	} else {
 		$cnt = 1;
 	};
+	
 	
 	if ( !$override ) {
 		foreach $ttnode ($tmpdoc->findnodes("//tok[\@torenum]")) {
@@ -92,18 +94,17 @@ use XML::LibXML;
 	}; 
 	if ( $verbose ) { print "\n - number of tokens: $cnt\n"; };
 
+
 	# Number the paragraphs
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//p")) {
-		$cnt++;
-		$ttnode->setAttribute('id', "p-$cnt");
+		addid($ttnode, "p", ++$cnt);
 	}; 
 	
 	# Number the mtoks
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//mtok")) {
-		$cnt++;
-		addid($ttnode, "m-$cnt")
+		addid($ttnode, "m", ++$cnt);
 	}; 
 	
 	# In case things have ended up as tei_div - rename
@@ -111,22 +112,16 @@ use XML::LibXML;
 		$ttnode->setName('div');
 	}; 
 	
-	
 	# Number the divs
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//div")) {
-		$cnt++;
-		if ( !$ttnode->getAttribute('id') || $force ) {
-			if ( $debug ) { print "Renumbering div-$cnt"; };
-			$ttnode->setAttribute('id', "div-$cnt");
-		};
+		addid($ttnode, "div", ++$cnt);
 	}; 
 	
 	# Number the sentences
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//s | $mtxtelem//l")) {
-		$cnt++;
-		addid($ttnode, "s-$cnt")
+		addid($ttnode, "s", ++$cnt);
 	}; 
 
 	# In case we have empty, unnumbered sentences
@@ -156,49 +151,38 @@ use XML::LibXML;
 	# Number the utterances
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//u")) {
-		$cnt++;
-		if ( !$ttnode->getAttribute('id') || $force ) {
-			$ttnode->setAttribute('id', "u-$cnt");
-		};
+		$ttnode->setAttribute('id', "u", ++$cnt);
 	}; 
 	
 	# Number the breaks and other empty elements
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//pb | $mtxtelem//lb | $mtxtelem//cb | $mtxtelem//gap | $mtxtelem//deco | $mtxtelem//milestone")) {
-		$cnt++;
-		$ttnode->setAttribute('id', "e-$cnt");
+		addid($ttnode, "e", ++$cnt);
 	}; 
 	
 	# Number the footnotes
 	$cnt = 0;
-	if ( $debug ) { print "Finding toks : $mtxtelem//note\n"; };
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//note")) {
-		$cnt++;
-		addid($ttnode, "ftn-$cnt")
+		addid($ttnode, "ftn", ++$cnt);
 	}; 
 	
 	# Number the footnotes
 	$cnt = 0;
-	if ( $debug ) { print "Finding anon : $mtxtelem//anon\n"; };
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//anon")) {
-		$cnt++;
-		addid($ttnode, "an-$cnt")
+		addid($ttnode, "an", ++$cnt);
 	}; 
 	
 	# Number the critical elements
 	$cnt = 0;
 	foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//app")) {
-		$cnt++;
-		$ttnode->setAttribute('id', "app-$cnt");
+		$ttnode->setAttribute('id', "app", ++$cnt);
 	}; 
-
 	
 	# Number the named entities
 	$cnt = 1;
 	foreach $nerelm ( split(",", "term,placeName,persName,orgName,name,ner,ne") ) {
 		foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//$nerelm")) {
-			while ( $tmpdoc->findnodes("$mtxtelem//$nerelm\[\@id=\"ner-$cnt\"\]") ) { $cnt++; };
-			if ( !$ttnode->getAttribute('id') )  { $ttnode->setAttribute('id', "ner-$cnt"); };
+			addid($ttnode, "ner", ++$cnt)
 		}; 
 	};
 		
@@ -208,20 +192,19 @@ use XML::LibXML;
 		foreach $tmp ( split(",", $xx) ) {
 			if ( $debug ) { print "Finding custom element : $mtxtelem//$tmp\n"; };
 			foreach $ttnode ($tmpdoc->findnodes("$mtxtelem//$tmp")) {
-				$cnt++;
-				addid($ttnode, "xx", $cnt)
+				addid($ttnode, "xx", ++$cnt)
 			}; 
 		};
 	};
 	
 	$teitext = $tmpdoc->toString;
-		
 			
 print "\n-- renumbering complete\n";
-if ( $debug ) {
+if ( $test ) {
 	# binmode ( STDOUT, ":utf8" );
 	print $teitext;
 } else {
+	if ( $debug ) { print $teitext; };
 	open FILE, ">$filename" or die ("no such file: $filename");
 	# binmode ( FILE, ":utf8" );
 	print FILE $teitext;
@@ -230,17 +213,20 @@ if ( $debug ) {
 
 sub addid($xnode, $newid, $newcnt) { 
 	( $xnode, $newid, $newcnt ) = @_; 
-	if ( $newcnt ) { 
+	$oldid = $xnode->getAttribute('id');
+	if ( $newcnt && ( !$oldid || $override || $oldid eq "torenew" ) ) { 
 		$tmp = $newid.'-'.$newcnt;
-		while ( $tmpdoc->findnodes("//*[\@id=\"$tmp\"]") ) { 
-			$newcnt++;
-			$tmp = $newid.'-'.$newcnt;
+		# make sure IDs are unique
+		if ( !$override ) {
+			while ( $tmpdoc->findnodes("//*[\@id=\"$tmp\"]") ) { 
+				$tmp = $newid.'-'.++$newcnt;
+			};
 		};
 		$newid = $tmp;
 	}
-	$oldid = $xnode->getAttribute('id');
 	if ( $oldid eq "torenew" ) { $oldid = ""; $gotoid = $newid; print "NEWID: $gotoid"; };
-	if ( !$oldid || $force ) {
+	if ( !$oldid || $override ) {
+		if ( $debug ) { print "Setting ID to $newid\n"; };
 		$xnode->setAttribute('id', $newid);
 	};
 };
