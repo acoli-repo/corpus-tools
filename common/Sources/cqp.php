c1f83a503a68eeb1e750dda238d33871b9f8c4fc
Maarten Janssen <maartenpt@gmail.com>
2022-10-26 17:31:26 +0200

    added raw output to cqp/seq

--- common/Sources/cqp.php
+++ common/Sources/cqp.php
@@ -124,7 +124,7 @@
 		
 			<form action='index.php?action=$action&act=$act' method=post>
 				<textarea style='width: 100%; height: 200px;' name=query>{$_POST['query']}</textarea>
-				<p><input type=submit value='Execute'> <input type=checkbox name=raw value=1> Render as plain text
+				<p><input type=submit value='Execute'> <input type=checkbox name=raw value=1> Download as plain text
 			</form>";
 
 		if ( $_POST['query'] ) {
@@ -138,6 +138,7 @@
 		
 			if ( $_POST['raw'] ) {
 				header('Content-Type: text/plain; charset=utf-8');
+			header("Content-Disposition: attachment; filename=\"results.txt\"");
 				print $result; exit;
 			} else $maintext .= "<hr><h2>Result</h2><pre>".htmlentities($result)."</pre>";
 		};
