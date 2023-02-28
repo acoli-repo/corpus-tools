f65293bf84a7f9542309a92e49d7e4f8c6c2aa12
Maarten Janssen <maartenpt@gmail.com>
2023-02-28 17:22:10 +0100

    added query-CQL

---
+++ common/Sources/query-CQL.php
@@ -0,0 +1,39 @@
+<?php
+
+	function queryCQL( $query, $qid ) {
+		global $settings;
+		
+		$cqpcorpus = $settings['cqp']['corpus'] or $cqpcorpus = "tt-".$foldername;
+		if ( $settings['cqp']['subcorpora'] ) {
+			$subcorpus = $_SESSION['subc'] or $subcorpus = $_GET['subc'];
+			if ( !$subcorpus ) {
+				fatal("No subcorpus selected");
+			};
+			$_SESSION['subc'] = $subcorpus;
+			$cqpcorpus = strtoupper("$cqpcorpus-$subcorpus"); # a CQP corpus name ALWAYS is in all-caps
+			$cqpfolder = "cqp/$subcorpus";
+			$corpusname = $_SESSION['corpusname'] or $corpusname = "Subcorpus $subcorpus";
+			$subcorpustit = "<h2>$corpusname</h2>";
+			$subfolder = "/$subcorpus";
+		} else {
+			$cqpcorpus = strtoupper($cqpcorpus); # a CQP corpus name ALWAYS is in all-caps
+			$cqpfolder = $settings['cqp']['cqpfolder'] or $cqpfolder = "cqp";
+			$corpusname = $settings['cqp']['name'] or $corpusname = $cqpcorpus;
+		};
+
+
+		$metaquery = "Matches = $query;
+	tabulate Matches match text_id, match s_id, match[0]..matchend[0] id;";
+		file_put_contents("tmp/$qid.cql", $metaquery);
+		$cmd = "/usr/local/bin/cqp -r cqp -D $cqpcorpus -f 'tmp/$qid.cql' | perl -e 'while(<>) { s/ /,/g; print; };' > cache/$qid"; 
+		file_put_contents("tmp/query", $cmd);
+		shell_exec($cmd);
+		if ( !$debug ) { unlink("tmp/$qid.cql"); };
+	};
+
+	$about['CQL'] = "<p>The CQL option lets you search through the Corpus WorkBench indexed corpus in TEITOK.</p>";
+	$syntax['CQL'] = "https://cwb.sourceforge.io/files/CQP_Tutorial.pdf";
+	
+	$hints['CQL'] = '[form="a.*"]';
+		
+?>
