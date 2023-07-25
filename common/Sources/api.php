dc3cc3cac20763d66d48cda75ca69dccfc52bf79
Maarten Janssen <maartenpt@gmail.com>
2023-07-25 15:15:56 +0100

    toolroot improvement in api

--- common/Sources/api.php
+++ common/Sources/api.php
@@ -11,6 +11,8 @@
 	};
 	if ( !$toolroot ) $toolroot = "/home/git/teitok-tools/";
 	if ( !$username && is_array($_SESSION[$sessionvar.'-api']) ) $username = $_SESSION[$sessionvar.'-api']['username'];
+
+	if ( !file_exists("$toolroot/Scripts/conllu2teitok.pl") ) fatal("teitok-tools repository not found");
 	
 	header('Content-Type: application/json; charset=utf-8');
 
