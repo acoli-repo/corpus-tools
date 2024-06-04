e30f1aab3d5197651bcc354d95a1ad079f71229c
Maarten Janssen <maartenpt@gmail.com>
2024-06-04 13:17:22 +0200

    corrected problem preventing from annotations showing in QB

--- common/Sources/querybuilder.php
+++ common/Sources/querybuilder.php
@@ -445,13 +445,14 @@
 
 		# Deal with any stand-off annotation attributes (errors, etc.)
 		if ( is_array ( $settings['cqp']['annotations']))
-		foreach ( $settings['cqp']['annotations'] as $xatts ) {
+		foreach ( $settings['cqp']['annotations'] as $xkey => $xatts ) {
 			if ( !$xatts['display'] || ( $xatts['admin'] && !$username ) ) continue;
 			if ( $xatts['admin'] ) $adms = " class=adminpart";
 			$querytext .= "$hr<div$adms><h3>{%{$xatts['display']}}</h3><table  class=qbt >"; $hr = "<hr>";
 			foreach ( $xatts as $key => $item ) {
-				if ( !is_object($item) ) continue;
-				$xkey = "{$xatts['key']}_$key";
+				if ( !is_object($item) && !is_array($item)) { // We only want to show real children
+					continue;
+				};
 				$val = $item['long']."" or $val = $item['display']."";
 				if ( $item['type'] == "group" ) { 
 					$querytext .= "<tr><td>&nbsp;<tr><td colspan=2 style='text-align: center; color: #992000; font-size: 10pt; border-bottom: 1px solid #aaaaaa; border-top: 1px solid #aaaaaa;'>{%$val}";
