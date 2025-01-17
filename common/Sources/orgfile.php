c3e35ad58620851b8d8dd55771a2dd7c81c6e5d1
Maarten Janssen <mjanssen@Maartens-Mac-Studio.local>
2025-01-17 13:58:51 +0100

    added viewers to orgfile

--- common/Sources/orgfile.php
+++ common/Sources/orgfile.php
@@ -54,7 +54,8 @@
 	if ( $viewers[$extention] ) {
 		
 		$vact = $viewers[$extention]['action'];
-		$rawtxt = "<p>User visualization tool: <a href='index.php?action=$vact&id=$filename'>$vact</a></p>";	
+		$xmlname = $ttxml->xmlid or $xmlname = preg_replace("/.*([^\/]+?)\?[^.]*/", "\\1", $filename);
+		$rawtxt = "<p>Custom visualization tool: <a href='index.php?action=$vact&id=$filename'>$vact</a></p>";	
 
 	} else if ( file_exists($filename) && !$filetype[$extention]['show'] ) {
 		
